fd872b2168cf7914edcd0e11002b34c6fe10ada9
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-12-07 14:39:55 -0800

    Added install scripts, havent tested yet

---
+++ install.sh
@@ -0,0 +1,127 @@
+#!/usr/local/bin/zsh
+# Credit: https://github.com/codeinthehole
+# Resources:
+# https://gist.github.com/codeinthehole/26b37efa67041e1307db
+# https://sourabhbajaj.com/mac-setup/Homebrew/Cask.html
+
+"Starting installing"
+
+# Install Xcode command line tool
+xcode-select --install
+
+# Install Homebrew if not installed
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew updat
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed
+brew install gnu-tar
+brew install gnu-indent
+brew install gnu-which
+brew install gnu-grep
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 5
+brew install bash
+
+PACKAGES=(
+	ack
+	autoconf
+	automake
+	boot2docker
+	gettext
+	git
+	graphviz
+    go
+    heroku
+	hub
+	jq
+	libjpeg
+	markdown
+	npm
+	pkg-config
+	postgresql
+	python
+    ruby
+    rust
+	ssh-copy-id
+	the_silver_searcher
+	tmux
+	tree
+	vim
+	wget
+    zsh
+	# ffmpeg
+	# imagemagick
+	# libmemcached 
+	# memcached
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASK=(
+    adobe-creative-cloud
+    canon-eos-utility
+    docker
+    firefox
+    google-chrome
+    haskell-for-mac
+    iterm2
+    mactex
+    macvim
+    malwarebytes
+    spotify
+    vagrant
+    vagrant-manager
+    virtualbox
+    visual-studio-code
+    vlc
+)
+
+echo "Installing cask apps..."
+brew cask install ${CASK[@]}
+
+echo "Installing fonts..."
+brew tap caskroom/fonts
+FONTS=(
+    font-cascadia
+    font-fira-code
+    font-fontawesome
+    font-inconsolata
+    font-input
+    font-montserrat
+    font-roboto
+    font-source-code-pro
+    font-source-sans-pro
+)
+
+brew cask Install ${FONT[@]}
+
+echo "Installing Python packages..."
+PYTHON_PACKAGES=(
+    pipenv
+)
+sudo pip install ${PYTHON_PACKAGES[@]}
+
+echo "Installing global npm packages..."
+NPM_PACKAGES=(
+    puppeteer
+    safe-rm
+)
+npm install ${NPM_PACKAGES[@]}
