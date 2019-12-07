c19130ff2959a15f64e8b784eec94bdea3d695dd
jerrypeng0112 <jerrypeng0112@hotmail.com>
2019-12-06 23:49:07 -0800

    Added credit and resources

--- pull-dotfiles.sh
+++ pull-dotfiles.sh
@@ -1,19 +1,24 @@
 #!/usr/local/bin/zsh
+# Credit: https://github.com/durdn
+# Resources:
+# https://www.atlassian.com/git/tutorials/dotfiles
 
 function dotfiles {
     /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
+echo "Pulling dotfiles"
+
 git clone --bare https://github.com/jerry-peng/dotfiles.git $HOME/.dotfiles
 dotfiles checkout
 
-if [ $? = 0 ]; then
-    echo "Checked out dotfiles";
-else
-    echo "Backing up pre-existing dotfiles";
+if [ $? != 0 ]; then
+    echo "Backing up pre-existing dotfiles..."
     mkdir -p .dotfiles-backup
     dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi
 
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
+
+echo "Finished pulling dotfiles"
