ef1a352991d306f9b1d1cba13b9590b9a4ffbfee
Jerry Peng <jerrypeng@Jerrys-MacBook-Pro.local>
2021-06-04 22:55:50 -0700

    Added ssh setup script + ssh config file

---
+++ setup-ssh.sh
@@ -0,0 +1,20 @@
+echo "Setting up ssh keys"
+
+read -p "Enter email for SSH key: " email
+
+ssh-keygen -t ed25519 -C "$email"
+
+eval `ssh-agent -s`
+
+if [ -d "~/.ssh" ]
+then 
+  echo "Creating ~/.ssh directory"
+  mkdir -p ~/.ssh
+fi
+
+echo "Copying ssh config"
+cp ./files/ssh_config ~/.ssh/config
+
+ssh-add -K ~/.ssh/id_ed25519
+
+pbcopy < ~/.ssh/id_ed25519.pub
